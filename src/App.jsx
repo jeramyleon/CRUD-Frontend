1bb08fd1baecb42d274df6ae98bd24fdf2c7c793
jeramyleon <jeramyleon@gmail.com>
2025-07-01 16:21:08 -0400

    home page skeleton created

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,16 @@ const App = () => {
     <div>
       <NavBar />
       <div className="app">
-        <h1>Hello React!</h1>
+        <h1 class="TTP">TTP</h1>
         <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
-
+        <p className="header">Welcome to our CRUD App!</p>
+        <p className="brief">This project was created through the collaboration of Bilal Burton, Jeramy Leon, and Webbney Vallon.<br></br>
+          The app was designed to help you find and register for NYC college campuses. We hope you enjoyed our homepage.<br></br>
+          When you're ready, please click the 'Search' button located at the top right of the screen, which will direct you to our search page.<br></br>
+          Thank you for visiting!</p>
+        <footer>All Rights Reserved. ©2025</footer>
+        <img class="campus-image" src="/campus.jpg"></img>
+        <button className="searchButton">Search</button>
         <Routes>{/* Currently, we don't have any routes defined */}</Routes>
       </div>
     </div>
