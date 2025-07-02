b0f0cc6685d37494660c469b0efd411e4f20eb2e
jeramyleon <jeramyleon@gmail.com>
2025-07-02 12:00:12 -0400

    removed homepage elements from app.jsx and made it a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./AppStyles.css";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Routes } from "react-router";
+import { BrowserRouter, BrowserRouter as Router, Routes } from "react-router";
 import Footer from "./components/footer";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
     <div className="total">
 
-      <NavBar />
-
-      <div className="app">
-
-        <h1 className="introduction">Welcome to our CRUD App!</h1>
-
-        <p className="brief">
-          This project was created through the collaboration of Bilal Burton, Jeramy Leon, and Webbney Vallon.<br></br>
-          The app was designed to help you find and add colleges and students. We hope you enjoyed our homepage.<br></br>
-          Whenever you're ready, please click any of the 'Search' buttons located at the top of the page, which will direct you to our search pages.<br></br>
-          Thank you for visiting!
-        </p>
-
-        <img class="campus-image" src="/campus.jpg"></img>
-
-        
-
-        <Routes>{/* Currently, we don't have any routes defined */}</Routes>
-      
-      </div>
-
+      <NavBar/>
+      <HomePage/>
       <Footer/>
 
     </div>
@@ -42,7 +24,7 @@ const App = () => {
 // declare Routes, Links, and use useful hooks like useNavigate.
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
