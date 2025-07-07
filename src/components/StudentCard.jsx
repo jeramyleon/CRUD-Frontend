e6a622724affa0345dec34cbed3e78633a1d68a6
jeramyleon <jeramyleon@gmail.com>
2025-07-07 00:10:29 -0400

    studentCard name now links to single page view

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./StudentCardStyles.css";
+import { Link } from "react-router";
 
 const StudentCard = ({ name, campus, picture}) => {
   return (
     <div className="Box">
         <div className="InternalBox">
             <img id="studentPicture" src={picture} alt={`${name}'s picture`}/>
-            <h2 id="studentName">{name}</h2>
+            <Link to="/singleStudent">
+              <h2 id="studentName">{name}</h2>
+            </Link>
             <p id="campusName">{campus}</p>
         </div>
     </div>
